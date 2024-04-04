05981dbe37eafe94b4098927fe922b2fb06adb59
Sean <ussa.say@gmail.com>
2024-04-04 09:03:16 -0800

    comment people who have committed

--- random-javaProject-LmaoHaha/beginning-of-the-day.js
+++ random-javaProject-LmaoHaha/beginning-of-the-day.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-console.error('right now is ', new Date())
+console.error('right now is ', new Date(), new Date().toLocaleString(), new Date().toString())
 const today = new Date()
 console.log(new Date(today.getFullYear(), today.getMonth(), today.getDate()).toISOString())
--- random-javaProject-LmaoHaha/read-commits.js
+++ random-javaProject-LmaoHaha/read-commits.js
@@ -1,7 +1,15 @@
 import fs from 'fs'
 
 const commits = JSON.parse(fs.readFileSync('commits.json', 'utf-8'))
-console.log(commits)
+//console.log(commits)
 
-console.log('committed today')
-console.log(commits.map(commit => commit.author?.login))
+//console.log('committed today')
+//console.log()
+const committers = new Set(commits.map(commit => commit.author?.login))
+
+fs.writeFileSync('issuemsg.txt', `these people have committed already: ${[...committers].join(', ')}`)
+
+const BASH_TRUE = 0
+const BASH_FALSE = 1
+
+process.exit(committers.size === 0 ? BASH_TRUE : BASH_FALSE)
