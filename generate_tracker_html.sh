a16fc0221505334d5603038bd647e4266de6b253
Sean Perry <sean.hyatt.perry@gmail.com>
2024-04-07 13:59:09 -0700

    Create leaderboard for gitcommits

--- generate_tracker_html.sh
+++ generate_tracker_html.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Print the input provided as arguments
+echo "test"
+echo "$@"
 # Loop through each argument passed to the script
 for arg in "$@"; do
     # Print each argument
     echo "$arg"
-    echo "diff arg"
 done
