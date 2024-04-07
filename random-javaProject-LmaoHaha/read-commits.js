db8aa7b2602a9d06e351c5025a1bde25f12925ad
Sean <ussa.say@gmail.com>
2024-04-06 22:14:45 -0800

    mention users who havent committed yet

--- random-javaProject-LmaoHaha/read-commits.js
+++ random-javaProject-LmaoHaha/read-commits.js
@@ -1,13 +1,22 @@
 import fs from 'fs'
 
 const commits = JSON.parse(fs.readFileSync('commits.json', 'utf-8'))
-//console.log(commits)
+console.log(commits)
 
 //console.log('committed today')
 //console.log()
 const committers = new Set(commits.map(commit => commit.author?.login))
+const everyone = Array.from(
+  fs.readFileSync('README.md', 'utf-8').matchAll(/\(https:\/\/github.com\/([\w-]+)\/?\)/g),
+  ([, username]) => username
+)
+const AT = process.env.DONT_PING === 'true' ? '' : '@'
 
-fs.writeFileSync('issuemsg.txt', `these people have committed already: ${[...committers].join(', ')}`)
+fs.writeFileSync('issuemsg.txt', [
+  `these people have committed already: ${[...committers].join(', ')}`,
+  `these people are in the repo: ${everyone.join(', ')}`,
+  `these people havent committed yet!! ${everyone.filter(user => !committers.has(user)).sort().map(u => `${AT}${u}`).join(', ')} go commit now!!`
+].join('\n\n'))
 
 const BASH_TRUE = 0
 const BASH_FALSE = 1
